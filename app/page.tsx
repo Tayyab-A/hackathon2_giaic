0763ad7922c8406675fc271f713f8334322ee397
muzaffar401 <ma9400667@gmail.com>
2024-12-11 20:42:59 +0500

    hackathon project

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
+import React from 'react'
+import Hero from './components/hero'
+import Card from './components/card'
+import Running from './components/running'
+import Gear from './components/gear'
+import Stan from './components/stan'
+import Essentials from './components/essentials'
 
-import Hero from "./components/hero";
-import Card from "./components/card";
-import Running from "./components/running";
-import Gear from "./components/gear";
-import Essentials from "./components/essentials";
-import Stan from "./components/stan";
-
-
-export default function Home() {
+const page = () => {
   return (
-    <main className="max-w-screen-2xl mx-auto">
+    <div>
+      <main className="max-w-screen-2xl mx-auto">
 
-      <Hero/>
-      <Card/>
-      <Running/>
-      <Gear/>
-      <Stan/>
-      <Essentials/>
-    </main>
-  );
+        <Hero />
+        <Card />
+        <Running />
+        <Gear />
+        <Stan />
+        <Essentials />
+      </main>
+    </div>
+  )
 }
+
+export default page
